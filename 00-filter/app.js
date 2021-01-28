2e237bf9c30ca78dad069cebc4f04aa7a0e5d0b0
Miu <miu@Mius-MacBook-Pro.local>
2021-01-28 12:30:31 -0800

    first commit

--- 00-filter/app.js
+++ 00-filter/app.js
@@ -1,10 +1,14 @@
 //get result element
+const result = document.querySelector('ul');
 //get input filter element
+const input = document.querySelector('input');
 //define an array variable
+const listItems = [];
 
 //add an event listener to filter element
 filter.addEventListener('input', function(e){
     // filterData(e.???.???);
+    filterData(e.target.value)
 })
 
 getData();
@@ -15,23 +19,36 @@ async function getData() {
 
     // Clear result
     result.innerHTML = ''
+    
+    for (const r of results) {
 
-    //iterate every result and display inside of UL
-    // <img src=?? 
-    // <div class="user-info"
-        //h4 --- name
-        //p -- any info you wanna put
-    // </div
+        const li = document.createElement('li');
 
+        li.innerHTML = `
+            <img src=${r.picture.thumbnail} />
+            <div class="user-info">
+              <h4>${r.name.first}</h4>
+            <p>${r.location.city}</p>
+            </div>
+        `;
+
+        result.appendChild(li);
+
+        listItems.push(li);
+    }
 }
+
+
 
 function filterData(searchTerm) {
     listItems.forEach(item => {
         /* add conditional logic below */
-        if() {
+        if(item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
             //remove the class of .hide
+            item.classList.add('hide');
         } else {
             //add the class of .hide
+            item.classList.remove('hide');
         }
     })
 }
